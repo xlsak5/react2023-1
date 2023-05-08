09dc0690272b073d110d8e001e042377e6d42624
testAnGyonam <skadldldl123@gmail.com>
2023-05-08 13:04:32 +0900

    2023.05.08

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 자바스크립트로 했을때
+// function click() {
+//   let clock = document.getElementById("clock");
+
+//   setInterval(function(){
+//     clock.innerHTML = new Date().toLocaleDateString();
+//   }, 1000);
+// }
+// click();
+// export default clock;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <h1>리액트</h1>
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// // React와 ReactDOM을 선언해줘야 사용가능.
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // index.js는 자바스크립트 페이지
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+// 앞 글자가 대문자면 컴포넌트를 만들었다는 애기
+import App from "./App.js";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
